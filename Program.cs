c6ef90d6d23ca0dbb70775215389d7ffc9c48c09
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-10-19 21:05:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Var_Dynamic_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Var :- Store Any Type Of Data. But Var is compile Time. Once we assign Value at Compile time its fixed.
+            var varNum = 123;
+            var varData = " Sample Test";
+            Console.WriteLine(varData);
+
+            // Once Value assign then type getting fixed and cannot changed at runtime.
+            varData = 123;
+
+
+            //Dynamic : Store any type of data. we can change data at runime as well.
+                        dynamic dynData = "Sample Dynamic Data";
+            Console.WriteLine(dynData);
+
+            dynData = 123465789;
+            dynData = 12.326;
+            dynData = true;
+            dynData = "Dyanamic";
+
+
+        }
+    }
+}
